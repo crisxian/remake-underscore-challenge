905c6db168bde8f0a35d693f335c2b13d0b6b422
crisxian <crisxian@users.noreply.github.com>
2015-03-27 22:26:15 -0700

    every and contains functions passed

--- src/underbar.js
+++ src/underbar.js
@@ -185,6 +185,13 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+    return _.reduce(collection, function(pass, item) {
+      if(!pass) {
+        return false;
+      }
+      else return iterator(item)? true : false; // need bool
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
